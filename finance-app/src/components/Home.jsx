fa651428743dfb1ba0628b6378713ba1c77a3d6b
Jordan Wolter <jwolter456@gmail.com>
2022-11-04 12:38:43 -0400

    more styling done to home page

--- finance-app/src/components/Home.jsx
+++ finance-app/src/components/Home.jsx
@@ -43,28 +43,28 @@ export default function Home () {
                <a href={Home[0].url}target='_blank'><img src={Home[0].image}></img></a> 
                 
              </div>
-             <div>
+             <div className='story1'>
              <h1>{Home[1].title}</h1>
                 <h3>{Home[1].site}, {Home[1].publishedDate},     {Home[1].symbol}</h3>
                 <a href={Home[1].url}target='_blank'><img src={Home[1].image}></img></a>
                 
              </div> 
-             <div>
+             <div className='story2'>
              <h1>{Home[2].title}</h1>
                 <h3>{Home[2].site},  {Home[2].publishedDate},    {Home[2].symbol}</h3>
                 <a href={Home[2].url}target='_blank'><img src={Home[2].image}></img></a>
              </div> 
-             <div>
+             <div className='story3'>
              <h1>{Home[3].title}</h1>
                 <h3>{Home[3].site},  {Home[3].publishedDate},    {Home[3].symbol}</h3>
                 <a href={Home[3].url}target='_blank'><img src={Home[3].image}></img></a>
              </div> 
-             <div>
+             <div className='story4'>
              <h1>{Home[4].title}</h1>
                 <h3>{Home[4].site},  {Home[4].publishedDate},    {Home[4].symbol}</h3>
                 <a href={Home[4].url}target='_blank'><img src={Home[4].image}></img></a>
              </div> 
-             <div>
+             <div className='story5'>
              <h1>{Home[5].title}</h1>
                 <h3>{Home[5].site},  {Home[5].publishedDate},    {Home[5].symbol}</h3>
                 <a href={Home[5].url}target='_blank'><img src={Home[5].image}></img></a>
--- finance-app/src/components/Nav.jsx
+++ finance-app/src/components/Nav.jsx
@@ -4,8 +4,8 @@ export default function Nav () {
     return (
         <div className='navLinks'>
             <Link to='/'>Home</Link>
-            <Link to ='/finance'>Finance</Link>
             <Link to ='/company'>Company</Link>
+            <Link to ='/finance'>Finance</Link>
             <Link to ='/stocks'>Stocks</Link>
         </div>
     )
