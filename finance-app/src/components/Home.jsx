7679c1d36bc2fc80eed6deffb000a8801fa8ccd6
Jordan Wolter <jwolter456@gmail.com>
2022-11-07 15:40:33 -0500

    added 4 markets on top that display updating pricing

--- finance-app/src/components/Home.jsx
+++ finance-app/src/components/Home.jsx
@@ -39,6 +39,9 @@ export default function Home () {
 
 
     }, [])
+
+
+   
  
     //we need to see the data
     //also create a guard operator
@@ -49,117 +52,120 @@ export default function Home () {
         return <h2> Loading please wait</h2>
     } else {
         return (
-        <div className='storyGrid'>
-             <div className='story1'>
-               <a href={Home[1].url}target='_blank' className='alink1'>
-                  <h1 className='storyTitle1'>{Home[1].title}</h1>
-                  <h3 className='storyInfo1'>{Home[1].site}, {Home[1].publishedDate},     {Home[1].symbol}</h3>
-                  <img className='storyImg1' src={Home[1].image}></img>
-                  <p className='storyText'>{Home[1].text}</p>
-                </a>
-             </div> 
+         <div>
+          
+         <div className='storyGrid'>
+               <div className='story1'>
+                  <a href={Home[1].url}target='_blank' className='alink1'>
+                     <h1 className='storyTitle1'>{Home[1].title}</h1>
+                     <h3 className='storyInfo1'>{Home[1].site}, {Home[1].publishedDate},     {Home[1].symbol}</h3>
+                     <img className='storyImg1' src={Home[1].image}></img>
+                     <p className='storyText'>{Home[1].text}</p>
+                  </a>
+               </div> 
 
-             <div className='story2'>
-             <a href={Home[2].url}target='_blank' className='alink'>
-                  <h1 className='storyTitle'>{Home[2].title}</h1>
-                  <h3 className='storyInfo'>{Home[2].site},  {Home[2].publishedDate},    {Home[2].symbol}</h3>
-                  <img className='storyImg' src={Home[2].image}></img>
-                </a>
-             </div>
-
-             <div className='story3' >
-               <a href={Home[3].url}target='_blank' className='alink'>
-                  <h1 className='storyTitle'>{Home[3].title}</h1>
-                  <h3 className='storyInfo'>{Home[3].site},  {Home[3].publishedDate},    {Home[3].symbol}</h3>
-                  <img className='storyImg' src={Home[3].image}></img>
-                </a>
-             </div> 
-
-             <div className='story4'>
-               <a href={Home[4].url}target='_blank' className='alink'>
-                  <h1 className='storyTitle'>{Home[4].title}</h1>
-                  <h3 className='storyInfo'>{Home[4].site},  {Home[4].publishedDate},    {Home[4].symbol}</h3>
-                  <img className='storyImg' src={Home[4].image}></img>
-               </a>
-             </div> 
-
-             <div className='story5'>
-               <a href={Home[5].url}target='_blank' className='alink'>
-                  <h1 className='storyTitle'>{Home[5].title}</h1>
-                  <h3 className='storyInfo'>{Home[5].site},  {Home[5].publishedDate},    {Home[5].symbol}</h3>
-                  <img className='storyImg' src={Home[5].image}></img>
-               </a>
-             </div> 
-
-             <div className='story6'>
-               <a href={Home[6].url}target='_blank' className='alink'>
-                  <h1 className='storyTitle'>{Home[6].title}</h1>
-                  <h3 className='storyInfo'>{Home[6].site},  {Home[6].publishedDate},    {Home[5].symbol}</h3>
-                  <img className='storyImg' src={Home[6].image}></img>
-                </a>
-             </div> 
-
-               <div className='sectorReport'>
-                  <h1 className='sectorTitle'>Daily Sector Report</h1>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[11].sector}:</h2> 
-                     <h2 className={sector[11].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[11].changesPercentage}</h2>
-                  </div>
-                  
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[7].sector}: </h2>
-                     <h2 className={sector[7].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[7].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[6].sector}: </h2>
-                     <h2 className={sector[6].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[6].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[10].sector}:</h2>
-                     <h2 className={sector[10].changesPercentage.includes('-') ? 'negative' : 'positive'}> {sector[10].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[8].sector}: </h2>
-                     <h2 className={sector[8].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[8].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[5].sector}: </h2>
-                     <h2 className={sector[5].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[5].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[4].sector}: </h2>
-                     <h2 className={sector[4].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[4].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[1].sector}:</h2>
-                     <h2 className={sector[1].changesPercentage.includes('-') ? 'negative' : 'positive'}> {sector[1].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[3].sector}: </h2>
-                     <h2 className={sector[3].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[3].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[2].sector}: </h2>
-                     <h2 className={sector[2].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[2].changesPercentage}</h2>
-                  </div>
-
-                  <div className='sector11'>
-                     <h2 className='sectorTitleChange'>{sector[9].sector}: </h2>
-                     <h2 className={sector[9].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[9].changesPercentage}</h2>
-                  </div>
+               <div className='story2'>
+               <a href={Home[2].url}target='_blank' className='alink'>
+                     <h1 className='storyTitle'>{Home[2].title}</h1>
+                     <h3 className='storyInfo'>{Home[2].site},  {Home[2].publishedDate},    {Home[2].symbol}</h3>
+                     <img className='storyImg' src={Home[2].image}></img>
+                  </a>
                </div>
-             
 
-        </div>    
+               <div className='story3' >
+                  <a href={Home[3].url}target='_blank' className='alink'>
+                     <h1 className='storyTitle'>{Home[3].title}</h1>
+                     <h3 className='storyInfo'>{Home[3].site},  {Home[3].publishedDate},    {Home[3].symbol}</h3>
+                     <img className='storyImg' src={Home[3].image}></img>
+                  </a>
+               </div> 
+
+               <div className='story4'>
+                  <a href={Home[4].url}target='_blank' className='alink'>
+                     <h1 className='storyTitle'>{Home[4].title}</h1>
+                     <h3 className='storyInfo'>{Home[4].site},  {Home[4].publishedDate},    {Home[4].symbol}</h3>
+                     <img className='storyImg' src={Home[4].image}></img>
+                  </a>
+               </div> 
+
+               <div className='story5'>
+                  <a href={Home[5].url}target='_blank' className='alink'>
+                     <h1 className='storyTitle'>{Home[5].title}</h1>
+                     <h3 className='storyInfo'>{Home[5].site},  {Home[5].publishedDate},    {Home[5].symbol}</h3>
+                     <img className='storyImg' src={Home[5].image}></img>
+                  </a>
+               </div> 
+
+               <div className='story6'>
+                  <a href={Home[6].url}target='_blank' className='alink'>
+                     <h1 className='storyTitle'>{Home[6].title}</h1>
+                     <h3 className='storyInfo'>{Home[6].site},  {Home[6].publishedDate},    {Home[5].symbol}</h3>
+                     <img className='storyImg' src={Home[6].image}></img>
+                  </a>
+               </div> 
+
+                  <div className='sectorReport'>
+                     <h1 className='sectorTitle'>Daily Sector Report</h1>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[11].sector}:</h2> 
+                        <h2 className={sector[11].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[11].changesPercentage}</h2>
+                     </div>
+                     
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[7].sector}: </h2>
+                        <h2 className={sector[7].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[7].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[6].sector}: </h2>
+                        <h2 className={sector[6].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[6].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[10].sector}:</h2>
+                        <h2 className={sector[10].changesPercentage.includes('-') ? 'negative' : 'positive'}> {sector[10].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[8].sector}: </h2>
+                        <h2 className={sector[8].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[8].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[5].sector}: </h2>
+                        <h2 className={sector[5].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[5].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[4].sector}: </h2>
+                        <h2 className={sector[4].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[4].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[1].sector}:</h2>
+                        <h2 className={sector[1].changesPercentage.includes('-') ? 'negative' : 'positive'}> {sector[1].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[3].sector}: </h2>
+                        <h2 className={sector[3].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[3].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[2].sector}: </h2>
+                        <h2 className={sector[2].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[2].changesPercentage}</h2>
+                     </div>
+
+                     <div className='sector11'>
+                        <h2 className='sectorTitleChange'>{sector[9].sector}: </h2>
+                        <h2 className={sector[9].changesPercentage.includes('-') ? 'negative' : 'positive'}>{sector[9].changesPercentage}</h2>
+                     </div>
+                  </div>
+               
+
+         </div> 
+         </div>   
         )
     }
 
--- finance-app/src/components/Nav.jsx
+++ finance-app/src/components/Nav.jsx
@@ -1,14 +1,52 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Nav () {
+    const [market, setMarket] =useState(null)
+ 
+    useEffect(() => {
+       
+        const getMarket = async () => {
+        const response = await axios.get(`https://financialmodelingprep.com/api/v3/quotes/index?apikey=91c950cb2afdc944512490e8ae2113cc`)
+       
+        console.log(response.data)
+        setMarket(response.data)
+        }
+        getMarket()
+
+
+    }, [])
+    if (!market) {
+        return <h2> Loading please wait</h2>
+    } else {
     return (
-        <div className='navLinks'>
+        <div className='wrapper'>
+            
+            <div className='navlinks2'>
             <Link to='/' className='linkz'>Home</Link>
             <Link to ='/company' className='linkz'>Company</Link>
             <Link to ='/finance' className='linkz'>Stock Search</Link>
             <Link to ='/stocks' className='linkz'>Stocks</Link>
-            
+            </div>
+
+            <div className='markets'>
+                <h3>{market.find(e => e.name === 'Dow Jones Industrial Average').name}: {market.find(e => e.name === 'Dow Jones Industrial Average').price}     {market.find(e => e.name === 'Dow Jones Industrial Average').changesPercentage}%</h3>
+                
+                <h3>{market.find(e => e.name === 'S&P 500').name}:  
+                {market.find(e => e.name === 'S&P 500').price}
+                {market.find(e => e.name === 'S&P 500').changesPercentage}%</h3>
+
+                <h3>{market.find(e => e.name === 'NASDAQ Composite').name}:
+                {market.find(e => e.name === 'NASDAQ Composite').price}
+                {market.find(e => e.name === 'NASDAQ Composite').changesPercentage}</h3>
+
+                <h3>{market.find(e => e.name === 'Russell 2000').name}
+                {market.find(e => e.name === 'Russell 2000').price}
+                {market.find(e => e.name === 'Russell 2000').changesPercentage}</h3>
+            </div>
             
         </div>
     )
+    }
 }
