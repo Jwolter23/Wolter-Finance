0e06f66f63e5d90904984332c7331f13e78e75e7
Jordan Wolter <jwolter456@gmail.com>
2022-11-09 10:03:19 -0500

    added in a loser and active section on the active page

--- finance-app/src/components/Stocks.jsx
+++ finance-app/src/components/Stocks.jsx
@@ -25,6 +25,48 @@ export default function Stocks () {
         getGainer()
     }, [])
     
+
+
+    const [loser, setLoser] =useState(null)
+    //we need to call an axios function
+    useEffect(() => {
+        
+        const getLoser = async () => {
+        const response8 = await axios.get(`https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=91c950cb2afdc944512490e8ae2113cc`)
+        
+
+         //we need to set state of our data
+         console.log(response8.data)
+        setLoser(response8.data)
+        
+
+        }
+        getLoser()
+    }, [])
+
+
+
+    const [active, setActive] =useState(null)
+    
+    useEffect(() => {
+        
+        const getActive = async () => {
+        const response7 = await axios.get(` https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=91c950cb2afdc944512490e8ae2113cc`)
+        
+
+         console.log(response7.data)
+        setActive(response7.data)
+        
+
+        }
+        getActive()
+    }, [])
+
+
+
+
+
+   
    
 
     //we need to see the data
@@ -32,12 +74,13 @@ export default function Stocks () {
     //so if data takes a few seconds 
     //site doesnt break
 
-    if (!gainer) {
+    if (!gainer || !loser || !active) {
         return <h2> Loading please wait</h2>
     } else {
         return (
             
-               <div>
+               <div className='activityGrid'>
+                <div className='gainerDiv'>
                 <h1>Largest Gainers</h1>
                 {
                     gainer.map((gain) => (
@@ -48,7 +91,34 @@ export default function Stocks () {
                         </div>
                         
                     ))
+
+                    
                 }
+                </div>
+                <div className='loserDiv'>
+                <h1>Largest Losers</h1>
+                {
+                    loser.map((lose) => (
+                        <div>
+                            <h2>{lose.name}</h2> <h5>{lose.symbol}</h5>
+                            <h4>${lose.price} {lose.change}%</h4>
+                        </div>
+                    ))
+                }
+                </div>
+
+
+                <div className='activeDiv'>
+                <h1>Most Active</h1>
+                {
+                    active.map((act) => (
+                        <div>
+                            <h2>{act.name}</h2> <h5>{act.symbol}</h5>
+                            <h4>${act.price} {act.change}%</h4>
+                        </div>
+                    ))
+                }
+                </div>
                </div>
 
                 
